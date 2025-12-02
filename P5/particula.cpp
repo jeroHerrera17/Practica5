e87b7e26e82f3db80d35f2ea61b61027447fb860
jeroHerrera17 <jherrera.espinosa@udea.edu.co>
2025-12-02 17:46:37 -0500

    definimos los metodos de particula y vector 2D

--- P5/particula.cpp
+++ P5/particula.cpp
@@ -1,3 +1,69 @@
-#include "particula.h"
+#include "Particula.h"
+#include <iostream>
+#include <cmath>
 
-Particula::Particula() {}
+using namespace std;
+
+// Inicialización de variable estática
+int Particula::nextId = 0;
+
+// Constructor
+Particula::Particula(Vector2D pos, Vector2D vel, double m, double r)
+    : posicion(pos), velocidad(vel), masa(m), radio(r), activa(true) {
+    id = nextId++;
+}
+
+// Métodos públicos
+int Particula::getId() const {
+    return id;
+}
+
+void Particula::actualizar(double dt) {
+    if (activa) {
+        // Actualizar posición usando velocidad
+        // x(t + dt) = x(t) + v * dt
+        posicion = posicion + velocidad * dt;
+    }
+}
+
+void Particula::fusionarCon(Particula& otra) {
+    if (!activa || !otra.activa) {
+        return;
+    }
+
+    // Conservación del momento lineal
+    // p_total = m1*v1 + m2*v2
+    Vector2D momentoTotal = velocidad * masa + otra.velocidad * otra.masa;
+    double masaTotal = masa + otra.masa;
+
+    // Nueva velocidad del sistema fusionado
+    // v' = p_total / m_total
+    velocidad = momentoTotal / masaTotal;
+
+    // Nueva posición (centro de masa)
+    // x_cm = (m1*x1 + m2*x2) / (m1 + m2)
+    posicion = (posicion * masa + otra.posicion * otra.masa) / masaTotal;
+
+    // Nueva masa
+    masa = masaTotal;
+
+    // Nuevo radio (conservando área total)
+    // A_total = A1 + A2
+    // π*r'^2 = π*r1^2 + π*r2^2
+    // r' = sqrt(r1^2 + r2^2)
+    radio = sqrt(radio * radio + otra.radio * otra.radio);
+
+    // Desactivar la partícula absorbida
+    otra.activa = false;
+}
+
+// Utilidades
+void Particula::imprimirInfo() const {
+    cout << "Particula #" << id << ": ";
+    cout << "Pos=";
+    posicion.print();
+    cout << ", Vel=";
+    velocidad.print();
+    cout << ", Masa=" << masa << ", Radio=" << radio;
+    cout << ", Activa=" << (activa ? "Si" : "No") << endl;
+}
--- P5/vector2d.cpp
+++ P5/vector2d.cpp
@@ -1,3 +1,52 @@
-#include "vector2d.h"
+#include "Vector2D.h"
+#include <iostream>
+#include <cmath>
 
-Vector2D::Vector2D() {}
+using namespace std;
+
+// Constructores
+Vector2D::Vector2D() : x(0.0), y(0.0) {}
+
+Vector2D::Vector2D(double x, double y) : x(x), y(y) {}
+
+// Operadores sobrecargados
+Vector2D Vector2D::operator+(const Vector2D& v) const {
+    return Vector2D(x + v.x, y + v.y);
+}
+
+Vector2D Vector2D::operator-(const Vector2D& v) const {
+    return Vector2D(x - v.x, y - v.y);
+}
+
+Vector2D Vector2D::operator*(double scalar) const {
+    return Vector2D(x * scalar, y * scalar);
+}
+
+Vector2D Vector2D::operator/(double scalar) const {
+    if (scalar != 0.0) {
+        return Vector2D(x / scalar, y / scalar);
+    }
+    return Vector2D(0.0, 0.0);
+}
+
+// Operaciones vectoriales
+double Vector2D::dot(const Vector2D& v) const {
+    return x * v.x + y * v.y;
+}
+
+double Vector2D::magnitude() const {
+    return sqrt(x * x + y * y);
+}
+
+Vector2D Vector2D::normalize() const {
+    double mag = magnitude();
+    if (mag > 0.0) {
+        return *this / mag;
+    }
+    return Vector2D(0.0, 0.0);
+}
+
+// Utilidades
+void Vector2D::print() const {
+    cout << "(" << x << ", " << y << ")";
+}
